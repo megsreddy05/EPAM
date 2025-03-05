f62ccbf323f7a2eb42d5f1776fd9ec292cc6f92d
practiceapnacoll <tetalameghanareddy@gmail.com>
2025-03-05 11:28:33 +0530

    Pushing Node.js for first time

---
+++ nodejs/demo.js
@@ -0,0 +1,19 @@
+function addNumber(a, b) {
+    return a + b;
+}    
+
+function subtractNumber(a, b) {
+    return a - b;
+}
+
+function divideNumber(a, b) {
+    return a / b;
+}
+
+function multiplyNumber(a, b) {
+    return a * b;
+}    
+
+module.exports = { addNumber, subtractNumber, multiplyNumber, divideNumber };
+
+
---
+++ nodejs/demo1.js
@@ -0,0 +1,6 @@
+const operations= require('./demo.js');
+
+console.log(operations.addNumber(2,3));
+console.log(operations.subtractNumber(2,3));
+console.log(operations.multiplyNumber(2,3));
+console.log(operations.divideNumber(2,3));
